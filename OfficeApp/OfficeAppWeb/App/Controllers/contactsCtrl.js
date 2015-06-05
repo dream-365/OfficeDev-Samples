799f3436dff8cc7f02eefc410a0764248de3085c
dream-365 <support-jec@hotmail.com>
2015-06-05 10:55:44 +0800

    Office 365 Contact API demo

--- OfficeApp/OfficeAppWeb/App/Controllers/contactsCtrl.js
+++ OfficeApp/OfficeAppWeb/App/Controllers/contactsCtrl.js
@@ -1,9 +1,18 @@
 ﻿(function () {
-    app.controller('contactsCtrl', ['$scope', 'o365ApiSvc', function ($scope, o365ApiSvc) {
+    app.controller('contactsCtrl', ['$scope', 'o365ApiSvc', 'notificationSvc', function ($scope, o365ApiSvc, notificationSvc) {
         $scope.getContacts = function () {
             o365ApiSvc.getContacts()
                        .success(function (data, status, headers, config) {
-                           util.showNotification('Info:', 'get contacts succeed!');
+                           var list = '';
+
+                           for(var i = 0; i < data.value.length; i++)
+                           {
+                               list += data.value[i].DisplayName + ', ';
+                           }
+
+                           list = list.substring(0, list.length - 2);
+
+                           notificationSvc.info('Contacts: ' + list);
                        });
         }
     }]);
