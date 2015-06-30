b7704e49f151333f57d4a059d54b8ad40ccd0c6e
dream-365 <support-jec@hotmail.com>
2015-06-30 10:42:53 +0800

    angular + winjs + office

---
+++ OfficeApp/OfficeAppWeb/App/winjs-app.js
@@ -0,0 +1,15 @@
+﻿var vroot = 'http://try.buildwinjs.com';
+
+angular.module('sample', ['winjs']).controller("sampleController", function ($scope) {
+
+    $scope.items = [
+        { title: "Marvelous Mint", text: "Gelato", picture: vroot + "/images/fruits/60Mint.png" },
+        { title: "Succulent Strawberry", text: "Sorbet", picture: vroot + "/images/fruits/60Strawberry.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: vroot + "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: vroot + "/images/fruits/60Lemon.png" },
+        { title: "Creamy Orange", text: "Sorbet", picture: vroot + "/images/fruits/60Orange.png" },
+        { title: "Very Vanilla", text: "Ice Cream", picture: vroot + "/images/fruits/60Vanilla.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: vroot + "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: vroot + "/images/fruits/60Lemon.png" }
+    ];
+});
