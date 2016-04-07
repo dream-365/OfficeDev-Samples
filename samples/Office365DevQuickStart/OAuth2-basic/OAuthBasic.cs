f4f04e65cd9f7251ca96c17e7406b1a0e7abb5fe
Jeffrey Chen <support-jec@hotmail.com>
2016-04-07 10:13:22 +0800

    add missing files

---
+++ samples/Office365DevQuickStart/OAuth2-basic/OAuthBasic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2_basic
+{
+    class OAuthBasic
+    {
+        public void Run()
+        {
+            var oauth = new OauthConfiguration
+            {
+                Authority = "https://login.microsoftonline.com",
+                Tenant = "common",
+                ClientId = "24bb1725-54d9-4190-b58c-ba5347fb336e",
+                RedirectURI = "http://localhost"
+            };
+
+            var flow = new AuthorizationCodeGrantFlow();
+
+            flow.Go(oauth);
+        }
+    }
+}
